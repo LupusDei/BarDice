7521abf4dd2bc3bddf6b49afdc9d7268d98e3fc3
lupusdei <lupusdei108@gmail.com>
2014-01-16 19:15:43 -0500

    Initial Commit

---
+++ BarDice/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BarDice
+//
+//  Created by Justin Martin on 1/16/14.
+//  Copyright (c) 2014 JMM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JMMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JMMAppDelegate class]));
+    }
+}
---
+++ BarDiceTests/BarDiceTests.m
@@ -0,0 +1,34 @@
+//
+//  BarDiceTests.m
+//  BarDiceTests
+//
+//  Created by Justin Martin on 1/16/14.
+//  Copyright (c) 2014 JMM. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BarDiceTests : XCTestCase
+
+@end
+
+@implementation BarDiceTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
